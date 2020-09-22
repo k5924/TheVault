96f64c17a977dc679ffdeeb24a951b80a8e0196a
Kamran <56407135+k5924@users.noreply.github.com>
2020-09-22 13:26:30 +0100

    using class structure to interact with windows in application so i can retrieve data from any fields in the app. changed folder structure of src so its easier to find certain items changed startPage.ui to use QWidget instead of QMainWindow as pyuic5 keeps generating extra code when used with the ui files with QMainWindow generated startPage.py now uses QWidget instead of QMainWindow which reduces the code in the file significantly.

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import sys
 from startPage import Ui_startPage
 from PyQt5 import QtWidgets
 
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_startPage()
+        self.ui.setupUi(self)
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    startPage = QtWidgets.QMainWindow()
-    ui = Ui_startPage()
-    ui.setupUi(startPage)
+    startPage = MainWindow()
     startPage.show()
     sys.exit(app.exec_())
