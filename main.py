c1a71b8d77e85a178bdef96511e83daca18a0b71
Kamran <56407135+k5924@users.noreply.github.com>
2020-09-24 17:25:45 +0100

    added QtCore to use QUrl added unpad to check that encrypted data is readable when opened moved QMessageBox into a callable function that i can edit created functions to return the path for the key and vault files which update how the labels for these files look on the gui checking that the correct files have been selected and if they havent error messages are returned depending on the trigger event

--- main.py
+++ main.py
@@ -4,47 +4,86 @@ from platform import system
 from string import ascii_uppercase
 from startPage import Ui_startPage
 from test import Ui_Form
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_startPage()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self)   # initializes start page
         self.ui.startButton.clicked.connect(self.createVaultFiles)
+        self.ui.selectKeyFile.clicked.connect(self.getKeyFile)
+        self.ui.selectVaultFile.clicked.connect(self.getVaultFile)
         self.ui.openButton.clicked.connect(self.openVaultFiles)
+        # button variabls which execute a specific function
 
     def createVaultFiles(self):
         key = get_random_bytes(32)  # 32 bytes is 256 bits
         data = b''  # basic data for file to encrypt
-        desktopPath = getPathToDesktop()
+        desktopPath = getPathToDesktop()    # gets path to desktop
         keyFile = open(desktopPath + "\\key.bin", "wb")
         keyFile.write(key)  # writes encryption key to file
         keyFile.close
         cipher = AES.new(key, AES.MODE_CBC)
         ciphered_data = cipher.encrypt(pad(data, AES.block_size))
-        vaultFile = open(desktopPath + "\\vault.bin", "wb")
+        vaultFile = open(desktopPath + "\\vault.bin", "wb")     # creates vault file
         vaultFile.write(cipher.iv)
         vaultFile.write(ciphered_data)
         vaultFile.close()
-        message = QtWidgets.QMessageBox()
-        message.setWindowTitle("Process Completed")
-        message.setText("Created vault.bin and key.bin")
-        message.setIcon(QtWidgets.QMessageBox.Information)
-        message.setDefaultButton(QtWidgets.QMessageBox.Ok)
-        message.exec_()
+        Alert("Process Completed", QtWidgets.QMessageBox.Information, "Created vault.bin and key.bin")
+        # Alert function to reuse the code to generate a QMessageBox
+
+    def getKeyFile(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Open file', "~", "All Files (*)")  # lets user choose files from explorer
+        url = QtCore.QUrl.fromLocalFile(file[0])    # gets path to file and stores it as an object
+        self.ui.keyFileLabel.setText(url.fileName())    # adjusts file name in gui
+        self.ui.keyFileLabel.adjustSize()   # adjusts size of text wrapper for file name in gui
+        self.keyPath = file[0]  # makes keyPath accessible in all of MainWindow class
+
+    def getVaultFile(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Open file', "~", "All Files (*)")  # lets user choose files from explorer
+        url = QtCore.QUrl.fromLocalFile(file[0])    # gets path to file and stores it as an object
+        self.ui.vaultFileLabel.setText(url.fileName())   # adjusts file name in gui
+        self.ui.vaultFileLabel.adjustSize()     # adjusts size of text wrapper for file name in gui
+        self.vaultPath = file[0]    # makes vaultPath accessible in all of MainWindow class
 
     def openVaultFiles(self):
-        self.newWindow = generateOrViewAllAccounts()
-        self.newWindow.show()
-        self.hide()
+        keyFile = self.ui.keyFileLabel.text()
+        vaultFile = self.ui.vaultFileLabel.text()
+        if (keyFile == "Key File") or (vaultFile == "Vault File"):
+            # checks that a Key File or Vault file have been selected
+            Alert("Error", QtWidgets.QMessageBox.Critical,
+                  "Either one or no files were selected. Please select files to open the vault")
+            # Alert function to display error QMessageBox
+        else:
+            # exception handling
+            try:
+                readVaultFile = open(self.vaultPath, 'rb')  # Open the file to read bytes
+                iv = readVaultFile.read(16)  # Read the iv out - this is 16 bytes long
+                ciphered_data = readVaultFile.read()  # Read the rest of the data
+                readVaultFile.close()
+                readKeyFile = open(self.keyPath, 'rb')
+                key = readKeyFile.read()
+                cipher = AES.new(key, AES.MODE_CBC, iv=iv)  # Setup cipher
+                # Decrypt and then up-pad the result
+                self.data = unpad(cipher.decrypt(ciphered_data), AES.block_size)
+                # display new window for generating password or viewing accounts
+                self.newWindow = generateOrViewAllAccounts()
+                self.newWindow.show()   # show new window
+                self.hide()  # close old window
+            except ValueError:
+                Alert("Error", QtWidgets.QMessageBox.Critical, "Incorrect files selected")
+                # Alert function to show error message
 
 
 class generateOrViewAllAccounts(QtWidgets.QWidget):
+    # displays window when vault is open
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_Form()
@@ -52,6 +91,7 @@ class generateOrViewAllAccounts(QtWidgets.QWidget):
 
 
 def getPathToDesktop():
+    # path to desktop is different on windows and unix systems as on windows the drive the desktop is on can be changed
     if system() == 'Windows':
         desktopPath = os.environ["HOMEPATH"] + "\Desktop"  # finds path to desktop
         for driveLetter in ascii_uppercase:  # find drive desktop folder is on
@@ -62,7 +102,17 @@ def getPathToDesktop():
     return desktopPath
 
 
+def Alert(title, icon, text):
+    # creates QMessageBox based on arguements in function
+    message = QtWidgets.QMessageBox()
+    message.setWindowTitle(title)
+    message.setIcon(icon)
+    message.setText(text)
+    message.exec_()
+
+
 if __name__ == "__main__":
+    # displays when starting application
     app = QtWidgets.QApplication(sys.argv)
     startPage = MainWindow()
     startPage.show()
